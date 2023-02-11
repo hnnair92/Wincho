1ae1c2dfedb073438a671e19611d07baa5f4a3e3
mohammedansil <anzilmohd4@gmail.com>
2023-02-12 02:36:12 +0530

    Gameplay Screen Curve Updated

--- src/Components/Description/Description.js
+++ src/Components/Description/Description.js
@@ -6,7 +6,9 @@ import { MdOutlineRestartAlt,MdFlipCameraIos } from "react-icons/md";
 import { RxExclamationTriangle } from "react-icons/rx";
 import user from '../../assests/penguin.png'
 import binoculars from '../../assests/binoculars.png'
-import svgIcon from '../../assests/sideIcon.png';
+import svgIcon from '../../assests/SIDEpng2.png';
+import svgBottom from '../../assests/SIDEBottom.png';
+// import svgIcon from '../../assests/sideIcon.png';
 const Description = () => {
 
 
@@ -41,11 +43,14 @@ const Description = () => {
           
           
           <div className={style.GamePlay}>
+              <div className={style.sideIcon}>
+                <img src={svgIcon} alt=""  className={style.svgRight}/>
+                <img src={svgBottom} alt="" className={style.svgBottom} />
+                  {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,256L120,229.3C240,203,480,149,720,149.3C960,149,1200,203,1320,229.3L1440,256L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"></path></svg> */}
+              </div>
               <div className={style.Screen}>
                 {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#000" fill-opacity="1" d="M 400 100 Q 500 300 400 500 L 500 500 L 500 100 L 400 100 "></path></svg> */}
-                <div className={style.sideIcon}>
-                    {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,256L120,229.3C240,203,480,149,720,149.3C960,149,1200,203,1320,229.3L1440,256L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"></path></svg> */}
-                </div>
+                <img src={game.featured_image.large} alt="" />
                 {/* <img src="https://images.unsplash.com/photo-1675789652972-ee2040d2cc9a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8&auto=format&fit=crop&w=2000&q=60" alt="" /> */}
                 {/* <iframe src="https://www.youtube.com/embed/-CbyAk3Sn9I" title="Pac-Man Original (Arcade 1980)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
                 {/* <video ref={videoRef} poster="https://images.unsplash.com/photo-1675789652972-ee2040d2cc9a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8&auto=format&fit=crop&w=2000&q=60" controls>
