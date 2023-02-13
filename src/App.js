93d5cc7fd2e1174b0bac84e7a0aca7aa5da18855
mohammedansil <anzilmohd4@gmail.com>
2023-02-13 17:51:04 +0530

    Updated Routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Header from './Components/Header/Header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Description from './Components/Description/Description';
 import Loader from './Components/Loader/Loader';
+import Register from './Components/register/Register';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
@@ -15,7 +17,9 @@ function App() {
           <Route path="/" element={<Games/>}/>
           <Route path="/:category" element={<Games/>}/>
           <Route path="/game/:id" element={<Description/>}/>
-          <Route path="/load" element={<Loader/>}/>
+          {/* <Route path="/load" element={<Loader/>}/> */}
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/login" element={<Login/>}/>
         </Routes>
       </Router>
       {/* <Games/> */}
