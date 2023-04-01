26c2950952dfddc9e9a75b342dc64bdb89d5cc94
mohadanxil <anzil.m@immneos.com>
2023-04-01 15:20:53 +0530

    header updated

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -11,6 +11,7 @@ import { HiMenu } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 import {assets} from '../Description/assests'
 import { updateProfile } from "../../actions/user";
+
 import bandaiLogo from '../../assests/Bandai Namco Logo.png'
 
 // import { useSelector } from 'react-redux'
